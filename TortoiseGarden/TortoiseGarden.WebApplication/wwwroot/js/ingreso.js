bd1aaf7792ec4803cd497d21da83c7d3f8c2c5ea
Irina Cabrera <irina.-cabrera@hotmail.com>
2024-06-12 20:05:54 -0300

    Se modificó el formulario de ingreso y registro, se agregó ingreso.js.

---
+++ TortoiseGarden/TortoiseGarden.WebApplication/wwwroot/js/ingreso.js
@@ -0,0 +1,17 @@
+﻿var btnRegistrarme = document.getElementsByClassName("btn-Registrarme")[0];
+var contenedorRegistrarUsuario = document.getElementById("registrarUsuario");
+var contenedorIngresarUsuario = document.getElementById("ingresarUsuario");
+var containerForm = document.getElementsByClassName("containerForm")[0];
+var btnCancelar = document.getElementsByClassName("btn-Cancelar")[0];
+
+
+containerForm.removeChild(contenedorRegistrarUsuario);
+
+btnRegistrarme.addEventListener("click", () => {
+    containerForm.removeChild(contenedorIngresarUsuario);
+    containerForm.appendChild(contenedorRegistrarUsuario);
+});
+btnCancelar.addEventListener("click", () => {
+    containerForm.removeChild(contenedorRegistrarUsuario);
+    containerForm.appendChild(contenedorIngresarUsuario);
+});
