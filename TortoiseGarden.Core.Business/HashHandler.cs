ae97460a355d108b20b3567ef99a269b4a34d1cc
Irina Cabrera <irina.-cabrera@hotmail.com>
2024-06-12 12:22:48 -0300

    Se creó y agregó funcionalidad a la clase HashHandler y se crearon las clases UserBusiness, UserRepository.

---
+++ TortoiseGarden.Core.Business/HashHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace TortoiseGarden.Core.Business
+{
+    public class HashHandler
+    {
+
+        public byte[] GenerarSalt()
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                byte[] salt = new byte[16];
+                rng.GetBytes(salt);
+               
+                return salt;
+            }
+        }
+        public byte[] HashearClave(string password, byte[] salt)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltedPassword = new byte[passwordBytes.Length + salt.Length];
+
+            // Concatenar password y salt
+            Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
+            Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
+
+            //Hashear la concatenacion de password y salt
+            byte[] hashedBytes = SHA256.HashData(saltedPassword);
+
+            return hashedBytes;
+        }
+
+    }
+}
---
+++ TortoiseGarden.Core.Data/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TortoiseGarden.Core.Data
+{
+    public class UserRepository
+    {
+
+    }
+}
