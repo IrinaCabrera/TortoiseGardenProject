86570145309f035670aabed35987bd03365a4f9f
Irina Cabrera <irina.-cabrera@hotmail.com>
2024-06-18 05:11:48 -0300

    Se agregó la funcionalidad de obtener Stock para el id de un producto.

---
+++ TortoiseGarden/TortoiseGarden.API/Controllers/ProductoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TortoiseGarden.Core.Business;
+using TortoiseGarden.Core.Entities;
+
+namespace TortoiseGarden.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoController : ControllerBase
+    {
+        private readonly SaleBusiness sb;
+        private readonly PurchaseBusiness purb;
+
+        public ProductoController()
+        {
+            sb = new SaleBusiness();
+            purb = new PurchaseBusiness();
+
+        }
+
+        [HttpGet("{IdProducto:int}/stock")]
+        public int ObtenerStockProducto(int IdProducto)
+        {
+            int cantComprada = purb.ObtenerCantidadDeProductoCompra(IdProducto);
+            int cantidadVendida = sb.ObtenerCantidadDeProductoVenta(IdProducto);
+
+            return cantComprada - cantidadVendida;
+            
+        }
+    }
+}
